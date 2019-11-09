1ab8e73e7ff7125baca6985cac776fffe9931efd
hourlier <hourlieradrien@gmail.com>
2019-11-09 01:20:34 -0600

    added a few histograms, trying to figure out what entry is what in the TOneTrackChunk of the neutrino preselection

--- MiniBooNE_neutrino.C
+++ MiniBooNE_neutrino.C
@@ -36,6 +36,9 @@ void MiniBooNE_neutrino::Loop()
    TH1D *hEMichel = new TH1D("hEMichel","hEMichel",100,0,1250);
    TH1D *hEmu = new TH1D("hEmu","hEmu",100,0,1250);
    TH1D *hEe = new TH1D("hEe","hEe",100,0,1250);
+   TH1D *hE_e_hyp = new TH1D("hE_e_hyp","hE_e_hyp",100,0,1250);
+
+   TH2D *hE01_e = new TH2D("hE01_e","hE01_e",100,0,1250,100,0,1250);
 
    TH2D *hEvparttype = new TH2D("hEvparttype","hEvparttype",100,0,1250,10,0,5);
    TH2D *hpartType01 = new TH2D("hpartType01","hpartType01",10,0,5,10,0,5);
@@ -45,16 +48,22 @@ void MiniBooNE_neutrino::Loop()
    TH1D *hE[kMaxTOneTrackChunk];
    TH1D *hTrackTypes[kMaxTOneTrackChunk];
    for(int i=0;i<kMaxTOneTrackChunk;i++){
-     hE[i] = new TH1D(Form("hE_%d",i),Form("hE_%d",i),1000,0,1000);
+     hE[i] = new TH1D(Form("hE_%d",i),Form("hE_%d",i),100,0,1000);
      hTrackTypes[i] = new TH1D(Form("hTrackTypes_%d",i),Form("hTrackTypes_%d",i),40,-10,10);
    }
    TH1D *hTrackType = new TH1D("hTrackType","hTrackType",40,-10,10);
+   
+   TH2D *hTOneTrackChunk_EvF_e    = new TH2D("hTOneTrackChunk_EvF_e"   ,"hTOneTrackChunk_EvF_e"   ,200,0,10,200,0,2000);
+   TH2D *hTOneTrackChunk_EvF_mich = new TH2D("hTOneTrackChunk_EvF_mich","hTOneTrackChunk_EvF_mich",200,0,10,200,0,2000);
+   TH2D *hTOneTrackChunk_EvF_mu   = new TH2D("hTOneTrackChunk_EvF_mu"  ,"hTOneTrackChunk_EvF_mu"  ,200,0,10,200,0,2000);
+
+
 
    for (Long64_t jentry=0; jentry<nentries;jentry++) {
       Long64_t ientry = LoadTree(jentry);
       if (ientry < 0) break;
       fChain->GetEntry(jentry);
-      std::cout << jentry << "/" << nentries << " = " << jentry*100./nentries << "% \t : \t" << TOneTrackChunk_data__E[1] << std::endl;      
+      //std::cout << jentry << "/" << nentries << " = " << jentry*100./nentries << "% \t : \t" << TOneTrackChunk_data__E[1] << std::endl;      
 
       for(int i=0;i<kMaxTOneTrackChunk;i++){
 	hE[i]->Fill(TOneTrackChunk_data__E[i]);
@@ -62,14 +71,27 @@ void MiniBooNE_neutrino::Loop()
 	hTrackType->Fill(TOneTrackChunk_data__trackType[i]);
 	hEvparttype->Fill(TOneTrackChunk_data__E[i],TOneTrackChunk_data__trackType[i]);
 	if(TOneTrackChunk_data__trackType[i] == 1){
-	  hEMichel->Fill(TOneTrackChunk_data__E[i]);
-	  hEe->Fill(TOneTrackChunk_data__E[i]);
+	  //hEMichel->Fill(TOneTrackChunk_data__E[i]);
+	  if(i != 1 && !(TOneTrackChunk_data__trackType[1] == 1 && TOneTrackChunk_data__E[1] < 60)){
+	    hEe->Fill(TOneTrackChunk_data__E[i]);
+	    hTOneTrackChunk_EvF_e->Fill(TOneTrackChunk_data__F[i],TOneTrackChunk_data__E[i]);
+	  }
 	}
 	if(TOneTrackChunk_data__trackType[i] == 2){
-	  hEmu->Fill(TOneTrackChunk_data__E[i]);	  
+	  hEmu->Fill(TOneTrackChunk_data__E[i]);
+	  hTOneTrackChunk_EvF_mu->Fill(TOneTrackChunk_data__F[i],TOneTrackChunk_data__E[i]);
 	}
+
+      }
+
+      if(TOneTrackChunk_data__trackType[1] == 1){
+	hEMichel->Fill(TOneTrackChunk_data__E[1]);
+	hTOneTrackChunk_EvF_mich->Fill(TOneTrackChunk_data__F[1],TOneTrackChunk_data__E[1]);
       }
       
+      hE_e_hyp->Fill(TOneTrackChunk_data__E[0]);
+      
+      if(TOneTrackChunk_data__trackType[0] == 1 && TOneTrackChunk_data__trackType[1] == 1)hE01_e->Fill(TOneTrackChunk_data__E[0],TOneTrackChunk_data__E[1]);
       
       hpartType12->Fill(TOneTrackChunk_data__trackType[1],TOneTrackChunk_data__trackType[2]);
       hpartType01->Fill(TOneTrackChunk_data__trackType[0],TOneTrackChunk_data__trackType[1]);
@@ -95,6 +117,9 @@ void MiniBooNE_neutrino::Loop()
      hE[i]->SetLineColor(i+2);
      hE[i]->Draw("same");
    }
+   hE_e_hyp->SetLineColor(2);
+   hE_e_hyp->SetLineWidth(2);
+   hE_e_hyp->Draw("same");
 
    TCanvas *cEvparttype = new TCanvas("cEvparttype","cEvparttype",800,600);
    hEvparttype->Draw("colz");
@@ -107,4 +132,16 @@ void MiniBooNE_neutrino::Loop()
 
    TCanvas *cpartType02 = new TCanvas("cpartType02","cpartType02",800,600);
    hpartType02->Draw("colz");
+
+   TCanvas *cTOneTrackChunk_EvF_e = new TCanvas("cTOneTrackChunk_EvF_e","cTOneTrackChunk_EvF_e",800,600);
+   hTOneTrackChunk_EvF_e->Draw("colz");
+
+   TCanvas *cTOneTrackChunk_EvF_mu = new TCanvas("cTOneTrackChunk_EvF_mu","cTOneTrackChunk_EvF_mu",800,600);
+   hTOneTrackChunk_EvF_mu->Draw("colz");
+
+   TCanvas *cTOneTrackChunk_EvF_mich = new TCanvas("cTOneTrackChunk_EvF_mich","cTOneTrackChunk_EvF_mich",800,600);
+   hTOneTrackChunk_EvF_mich->Draw("colz");
+
+   TCanvas *cE01_e = new TCanvas("cE01_e","cE01_e",800,600);
+   hE01_e->Draw("colz");
 }
